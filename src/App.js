64d4c2eeaa9feaa28e5412021bc7ec273a87cbd9
StephanyGarcesGuerra <stephanygarces96@gmail.com>
2024-02-09 14:57:27 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import logo from "./images/pandafit.avif"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> PandaFit</h1> 
+      <img src={logo} className="logo" alt="logo" />   
+      
+            
     </div>
   );
 }
